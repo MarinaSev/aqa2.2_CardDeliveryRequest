380cf61a8f66e41657da62b7c2d1f8ec4d5a0404
MarinaSev <pawpawsap@gmail.com>
2021-11-24 17:13:46 +0300

    date checking added to the Test

--- src/test/java/ru/netology/web/RequestFormTest.java
+++ src/test/java/ru/netology/web/RequestFormTest.java
@@ -3,18 +3,29 @@ package ru.netology.web;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Calendar;
 
 public class RequestFormTest {
 
-    //получаем теущую дату и прибавляем к ней 3 дня, переводим в строку
 
     @Test
-    public void shouldSendForm(){
+    public void shouldSendForm() {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.DATE, 3);
+        String meetingDay = dateFormat.format(calendar.getTime());
+
         open("http://localhost:9999");
         $(".input__control[placeholder='Город']").setValue("Киров");
-        $(".input__control[placeholder='Дата встречи']").shouldHave (exactValue("27.11.2021"));
+        $(".input__control[placeholder='Дата встречи']").shouldHave(exactValue(meetingDay));
         $(".input__control[name='name']").setValue("Иван Иванов");
         $(".input__control[name='phone']").setValue("+78889996633");
         $(".checkbox__box").click();
