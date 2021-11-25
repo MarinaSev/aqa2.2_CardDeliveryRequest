4b3198b3fd4b094e778844f105485ba8eba38c7b
MarinaSev <pawpawsap@gmail.com>
2021-11-25 15:05:55 +0300

    code looks better

--- src/test/java/ru/netology/web/RequestFormTest.java
+++ src/test/java/ru/netology/web/RequestFormTest.java
@@ -6,30 +6,37 @@ import static com.codeborne.selenide.Selenide.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Calendar;
 
+
 public class RequestFormTest {
+    private String meetingDay;
 
-
-    @Test
-    public void shouldSendForm() {
+    @BeforeEach
+    public void dateSetUp() {
         DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE, 3);
-        String meetingDay = dateFormat.format(calendar.getTime());
+        meetingDay = dateFormat.format(calendar.getTime());
+    }
 
+    @Test
+    public void shouldSendForm() {
         open("http://localhost:9999");
         $(".input__control[placeholder='Город']").setValue("Киров");
-        $(".input__control[placeholder='Дата встречи']").shouldHave(exactValue(meetingDay));
-        $(".input__control[name='name']").setValue("Иван Иванов");
+//        $(".input__control[placeholder='Дата встречи']").shouldHave(exactValue(meetingDay));
+        $(".input__control[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE, meetingDay);
+        $(".input__control[name='name']").setValue("Иванов Иван");
         $(".input__control[name='phone']").setValue("+78889996633");
         $(".checkbox__box").click();
         $(".button").click();
         $(withText("Успешно!")).shouldBe(appear, Duration.ofSeconds(15));
+//        $(".notification__content").getText().contains("Встреча успешно забронирована на ");
+//        $(".notification__content").getText().contains(meetingDay);
     }
 }
